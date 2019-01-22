624e185448e2e070f9af47708f4c6b3a64ffcc8f
Tiago B <tiago.gpes@gmail.com>
2019-01-22 18:20:17 -0200

    Primeiro commit

---
+++ app/src/main/java/app/teste/appprice/VisualizarSecaoActivity.java
@@ -0,0 +1,13 @@
+package app.teste.appprice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VisualizarSecaoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizar_secao);
+    }
+}
